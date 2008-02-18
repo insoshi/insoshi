067a622fef58625c75e476b2de97e6f3c9d7c92d
Michael Hartl <michael@michaelhartl.com>
2008-02-18 15:43:34 -0800

    Added Blueprint

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "home"
 
   # See how all your routes lay out with "rake routes"
 
