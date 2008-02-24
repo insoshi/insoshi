bc18306053214af45fd9c4a96ebc74061b2c7cf2
Michael Hartl <michael@michaelhartl.com>
2008-02-23 19:35:43 -0800

    Fixed infuriating MySQL decrypt bug The problem was the :limit => 40 in the person migration It didn't show up in SQLite...

--- db/migrate/001_create_people.rb
+++ db/migrate/001_create_people.rb
@@ -2,7 +2,7 @@ class CreatePeople < ActiveRecord::Migration
   def self.up
     create_table "people", :force => true do |t|
       t.string :email, :remember_token
-      t.string :crypted_password, :limit => 40
+      t.string :crypted_password
       t.datetime :remember_token_expires_at, :last_contacted_at,
                  :last_logged_in_at
       t.timestamps
@@ -11,7 +11,7 @@ class CreatePeople < ActiveRecord::Migration
   end
 
   def self.down
-    drop_table "people"
     remove_index :people, :email
+    drop_table "people"
   end
 end
--- db/migrate/002_remove_login_from_person.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLoginFromPerson < ActiveRecord::Migration
-  def self.up
-    remove_column :people, :login
-  end
-
-  def self.down
-    add_column :people, :login, :string
-  end
-end
