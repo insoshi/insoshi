517d7295a0e91ba17df11cb3ea4326de71073f66
Michael Hartl <michael@michaelhartl.com>
2008-02-23 17:33:42 -0800

    Email reminder test

---
+++ spec/controllers/email_reminders_controller_spec.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe EmailRemindersController do
+  
+  before(:each) do
+    ActionMailer::Base.delivery_method = :test
+    @emails = ActionMailer::Base.deliveries
+    @emails.clear    
+    @person = people(:quentin)
+  end
+  
+  it "should deliver a reminder" do
+    lambda do
+      post :create, :person => { :email => @person.email }
+      response.should be_redirect
+    end.should change(@emails, :length).by(1)
+  end
+end
