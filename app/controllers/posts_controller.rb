efce840b02208f76589ee9f266491bd6fb71ffee
Michael Hartl <michael@michaelhartl.com>
2008-02-25 17:42:42 -0800

    Laid foundation for blog & forum coexistence

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,104 @@
+class PostsController < ApplicationController
+  
+  before_filter :login_required
+  before_filter :get_instance_vars
+
+  def index
+    @posts = model.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def show
+    @post = model.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+    end
+  end
+
+  def new
+    @post = model.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+
+  def edit
+    @post = model.find(params[:id])
+  end
+
+  def create
+    data = { :topic => @topic, :person => current_person }
+    @post = model.new(params[:post].merge(data))
+    
+    respond_to do |format|
+      if @post.save
+        flash[:success] = 'Post was successfully created.'
+        format.html { redirect_to posts_url }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+
+  def update
+    @post = model.find(params[:id])
+
+    respond_to do |format|
+      if @post.update_attributes(params[:post])
+        flash[:success] = 'Post was successfully updated.'
+        format.html { redirect_to posts_url }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+
+  def destroy
+    @post = model.find(params[:id])
+    @post.destroy
+
+    respond_to do |format|
+      format.html { redirect_to posts_url }
+    end
+  end
+  
+  private
+  
+    def get_instance_vars
+      if forum?
+        @forum = Forum.find(params[:forum_id])
+        @topic = Topic.find(params[:topic_id])
+      end
+    end
+    
+    # Handle forum and blog posts in a uniform manner.
+    
+    def model
+      if forum?
+        ForumPost
+      elsif blog?
+        BlogPost
+      end
+    end
+    
+    def posts_url
+      if forum?
+        forum_topic_posts_url(@forum, @topic)
+      elsif blog?
+        blog_posts_url(@blog)
+      end
+    end
+    
+    def forum?
+      !params[:forum_id].nil?
+    end
+    
+    def blog?
+      !params[:blog_id].nil?
+    end
+end
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,45 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe PostsController do
+  integrate_views
+  
+  before(:each) do
+    @person = login_as(:quentin)
+    @forum = forums(:one)
+    @topic = topics(:one)
+  end
+  
+  it "should require login" do
+    logout
+    get :index
+    response.should redirect_to(login_url)
+  end
+  
+  it "should have working pages" do
+    with_options :forum_id => @forum, :topic_id => @topic do |page|
+      page.get    :index
+      page.get    :new
+      page.get    :show,    :id => posts(:one)
+      page.get    :edit,    :id => posts(:one)
+      page.post   :create,  :post => { :body => "The body" }
+      page.put    :update,  :id => posts(:one)
+      page.delete :destroy, :id => posts(:one)
+    end
+  end
+  
+  it "should associate a person to a post" do
+    with_options :forum_id => @forum, :topic_id => @topic do |page|
+      page.post :create, :post => { :body => "The body" }
+      assigns(:post).person.should == @person
+    end
+  end
+
+  it "should create a post" do
+    lambda do
+      post :create, :forum_id => @forum, :topic_id => @topic,
+                    :post => { :body => "The body" }
+      response.should redirect_to forum_topic_posts_url(@forum, @topic)
+    end.should change(Post, :count).by(1)
+  end
+
+end
