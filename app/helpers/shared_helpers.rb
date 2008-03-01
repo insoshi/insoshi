8748b94a2dbae575a59cfaa8fc4e2049ab7b3b4e
Michael Hartl <michael@michaelhartl.com>
2008-03-01 13:01:29 -0800

    Argh.  Fixed helper language.

--- app/helpers/shared_helpers.rb
+++
@@ -1,7 +0,0 @@
-# Helpers added to this module are available in both controllers and views.
-module SharedHelpers
-
-  def current_person?(person)
-    logged_in? and person == current_person
-  end
-end
