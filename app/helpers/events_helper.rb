5059943f18c4a9e3d0f951df349d920cdf6d9732
Michael Hartl <michael@michaelhartl.com>
2008-03-09 15:48:00 -0700

    BlogPostCommentEvent

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,24 +2,42 @@ module EventsHelper
 
   # Given an event, return a message for the feed for the event's class.
   def feed_message(event)
-    person = link_to(event.person.name, event.person)
+    person = event.person
+    # Switch on the class.to_s.  (The class itself is long & complicated.)
     case event.class.to_s
     when "BlogPostEvent"
       blog = event.post.blog
-      post_title = link_to(event.post.title, blog_post_path(blog, event.post))
-      msg = "#{person} has made a new blog post:<br /> #{post_title}"
+      view_blog = link_to("View #{person.name}'s blog", blog)
+      msg = %(#{person_link(person)} made a blog post titled
+              #{post_link(blog, event.post)}.
+              <br /> #{view_blog})
+    when "BlogPostCommentEvent"
+      post = event.comment.post
+      blog = post.blog
+      someones = current_person?(blog.person) ? 
+                 "their own" :
+                 "#{person_link(blog.person)}'s"
+      msg = %(#{person_link(person)} made a comment to
+              #{someones} blog post #{post_link(blog, post)})
     when "ConnectionEvent"
-      contact = link_to(event.conn.contact.name, event.conn.contact)
-      msg = "#{person} and #{contact} have connected"
+      contact_link = link_to(event.conn.contact.name, event.conn.contact)
+      msg = %(#{person_link(person)} and #{person_link(event.conn.contact)}
+              have connected)
     else
       "bar"
     end
     message_row(event, msg)
   end
 
+  def person_link(person)
+    link_to(person.name, person)
+  end
   
-  private
+  def post_link(blog, post)
+    link_to(post.title, blog_post_path(blog, post))
+  end
   
+  private  
     # Return a standard row element for a feed message.
     def message_row(event, message)
       markaby do
