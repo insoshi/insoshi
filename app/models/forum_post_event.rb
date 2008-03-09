56674bdb06c4d600c656888daf7ce4931ff25110
Michael Hartl <michael@michaelhartl.com>
2008-03-09 14:04:35 -0700

    Annotated models

--- app/models/forum_post_event.rb
+++ app/models/forum_post_event.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 10
+#
+# Table name: events
+#
+#  id          :integer(11)     not null, primary key
+#  person_id   :integer(11)     
+#  instance_id :integer(11)     
+#  type        :string(255)     
+#  created_at  :datetime        
+#  updated_at  :datetime        
+#
+
 class ForumPostEvent < Event
   belongs_to :post, :class_name => "ForumPost", :foreign_key => "instance_id"
 end
--- app/models/wall_comment_event.rb
+++ app/models/wall_comment_event.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 10
+#
+# Table name: events
+#
+#  id          :integer(11)     not null, primary key
+#  person_id   :integer(11)     
+#  instance_id :integer(11)     
+#  type        :string(255)     
+#  created_at  :datetime        
+#  updated_at  :datetime        
+#
+
 class WallCommentEvent < Event
   belongs_to :comment, :class_name => "WallComment",
                        :foreign_key => "instance_id"
