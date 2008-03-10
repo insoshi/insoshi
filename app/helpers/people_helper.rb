7c3ddbb6d2f0dd44c43a2cbb1afb226cf5c60f9d
Michael Hartl <michael@michaelhartl.com>
2008-03-09 21:40:49 -0700

    Captioned images

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,8 +3,21 @@ module PeopleHelper
   def image_links(people, options = {})
     image = options[:image] || :icon
     links = options[:links] || people
-    people.zip(links).map do |person, link|
-      link_to(image_tag(person.send(image)), link)
-    end
+    captions = options[:captions]
+    images = people.zip(links).map do |person, link|
+               link_to(image_tag(person.send(image)), link)
+             end
+    captions.nil? ? images : captioned(images, captions)
   end
+  
+  private
+    
+    # Make captioned images.
+    def captioned(images, captions)
+      images.zip(captions).map do |image, caption|
+        markaby do
+          image << div { caption }
+        end
+      end
+    end
 end
