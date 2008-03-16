9be5267fa595096abd0798a2b96fe123d4c2eb47
Michael Hartl <michael@michaelhartl.com>
2008-03-16 14:13:27 -0700

    Added Scribd-style PageView objects

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,8 +5,23 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   include AuthenticatedSystem
   include SharedHelper
+  
+  before_filter :create_page_view
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '71a8c82e6d248750397d166001c5e308'
+
+  private
+  
+    # Create a Scribd-style PageView.
+    # See http://www.scribd.com/doc/49575/Scaling-Rails-Presentation
+    def create_page_view
+      PageView.create(:user_id => session[:user_id],
+                      :request_url => request.request_uri,
+                      :session => session,
+                      :ip_address => request.remote_ip,
+                      :referer => request.env["HTTP_REFERER"],
+                      :user_agent => request.env["HTTP_USER_AGENT"])
+    end
 end
---
+++ app/models/page_view.rb
@@ -0,0 +1,2 @@
+class PageView < ActiveRecord::Base  
+end
