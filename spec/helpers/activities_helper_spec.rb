5b6fe1223887f18bb230bbc5f9bfd982f5cbb445
Kevin Ansfield <kev@digitalblueprint.co.uk>
2008-03-27 19:23:48 +0000

    Fix pluralization error in ActivitiesHelper spec

--- spec/helpers/activities_helper_spec.rb
+++ spec/helpers/activities_helper_spec.rb
@@ -3,9 +3,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe ActivitiesHelper do
   
   #Delete this example and add some real ones or delete this file
-  it "should include the ActivityHelper" do
+  it "should include the ActivitiesHelper" do
     included_modules = self.metaclass.send :included_modules
-    included_modules.should include(ActivityHelper)
+    included_modules.should include(ActivitiesHelper)
   end
   
 end
