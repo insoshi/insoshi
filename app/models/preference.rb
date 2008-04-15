8137752ddf67f1bd1d4b18e70b3a45d8dcd3dddb
Michael Hartl <michael@michaelhartl.com>
2008-04-15 12:06:30 -0700

    Added validations for email config if email notifications is set Also annotated the models

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+# Schema version: 16
+#
+# Table name: preferences
+#
+#  id                  :integer(11)     not null, primary key
+#  email_domain        :string(255)     
+#  smtp_server         :string(255)     
+#  email_notifications :boolean(1)      
+#  created_at          :datetime        
+#  updated_at          :datetime        
+#
+
 class Preference < ActiveRecord::Base
+  validates_presence_of :email_domain, :if => :email_notifications
+  validates_presence_of :smtp_server,  :if => :email_notifications  
+  
+  # Return true if we want the system to send out email validations.
+  # This is a stub for now; it's true if email_notifications is true.
+  # TODO: add finer-grained preferences
+  def email_validation?
+    email_notifications
+  end
 end
--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -2,10 +2,17 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Preference do
   before(:each) do
-    @preference = Preference.new
+    @preferences = Preference.new
   end
 
-  it "should be valid" do
-    @preference.should be_valid
+  it "should require email settings for email notifications" do
+    @preferences.email_notifications = true
+    @preferences.save.should be_false
+    @preferences.errors_on(:email_domain).should_not be_empty
+    @preferences.errors_on(:smtp_server).should_not be_empty
+  end
+  
+  it "should have a working email_validation boolean" do
+    @preferences.should respond_to(:email_validation?) 
   end
 end
