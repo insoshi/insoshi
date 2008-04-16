c96c424d676e1a2a39e1fc83736a18318a7d6836
Michael Hartl <michael@michaelhartl.com>
2008-04-15 19:45:57 -0700

    Fixed problem with migration & nonexistent preferences

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -1,9 +1,16 @@
-prefs = Preference.find(:first)
-if prefs.email_notifications? and not test?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address    => prefs.smtp_server,
-    :port       => 25,
-    :domain     => prefs.email_domain
-  }
+# Rescue from the error raised upon first migrating.
+begin
+  unless test?
+    prefs = Preference.find(:first)
+    if prefs.email_notifications?
+      ActionMailer::Base.delivery_method = :smtp
+      ActionMailer::Base.smtp_settings = {
+        :address    => prefs.smtp_server,
+        :port       => 25,
+        :domain     => prefs.email_domain
+      }
+    end
+  end
+rescue
+  nil
 end
--- db/migrate/017_add_default_preferences.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultPreferences < ActiveRecord::Migration
-  def self.up
-    Preference.create!(:email_notifications => false)
-  end
-
-  def self.down
-    Preference.find(:all).each {|p| p.destroy}
-  end
-end
