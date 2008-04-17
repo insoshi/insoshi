3aeecfb608df1afa557b0f301d672151f6b513f0
Michael Hartl <michael@michaelhartl.com>
2008-04-17 11:29:24 -0700

    Workaround for autotest/UUID interaction weirdness

--- config/initializers/custom_requires.rb
+++ config/initializers/custom_requires.rb
@@ -1,4 +1,16 @@
 require 'digest/sha1'
 require 'ferret_mixin'
 require 'rand'
-require 'uuid'
+
+# In some cases autotest interprets the initialization of the UUID generator
+# as something new, and so just keeps running the tests.
+# This stub here fixes the problem.
+unless test?
+  require 'uuid'
+else
+  class UUID
+    def self.new
+      Time.now.to_f.to_s
+    end
+  end
+end
