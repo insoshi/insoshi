5dd0bd0746580cc65c8efd4552a74a161ac65f4f
Jorge Dias <pana@qvitta.net>
2008-04-19 01:09:51 +0200

    Added break up link in contacts view

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,6 +3,7 @@ module PeopleHelper
   def image_links(people, options = {})
     image = options[:image] || :icon
     links = options[:links] || people
+    destroy = options[:destroy] || false
     captions = options[:captions]
     images = people.zip(links).map do |person, link|
                image_link(person, :link => link, :image => image)
@@ -11,8 +12,13 @@ module PeopleHelper
       images
     else
       captions = captions.zip(links).map { |c, l| link_to(c, l) } 
+      captions = captions.zip(breakup_links(people)).map { |c, l| c << l } if destroy
       captioned(images, captions)
     end
+  end
+  
+  def breakup_links(people)
+    a = people.map { |p| " | " << link_to("break up", current_person.connections.find_by_contact_id(p), :method => :delete, :confirm => "Do you want to break up with #{p.name}?") }
   end
 
   # Return a person's image link.
