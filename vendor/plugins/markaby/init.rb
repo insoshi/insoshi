9d5d96ef5be240a89cb54405d00dfb52683c6a8e
Michael Hartl <michael@michaelhartl.com>
2008-04-24 09:55:56 -0700

    Ripped out Markaby

--- vendor/plugins/markaby/init.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
-
-require 'markaby'
-require 'markaby/rails'
-
-ActionView::Base::register_template_handler 'mab', Markaby::Rails::ActionViewTemplateHandler
-
-ActionController::Base.send :include, Markaby::Rails::ActionControllerHelpers
--- vendor/plugins/markaby/test/rails/test_helper.rb
+++
@@ -1,7 +0,0 @@
-module TestHelper
-  def check_ivar_exists(name)
-    unless instance_variables.include?(ivar_name = "@#{name}")
-      raise "#{ivar_name} does not exist"
-    end
-  end
-end
