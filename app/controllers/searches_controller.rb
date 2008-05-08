36f34ca84b0d217e1d6b4b14a852a7a393fd2622
Michael Hartl <michael@michaelhartl.com>
2008-05-08 09:51:43 -0700

    Fixed exception at /searches (for Alexa's benefit)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
 
   def index
+    redirect_to home_url and return if params[:model].nil?
     model = params[:model]
     if model == "Message"
       options = params.merge(:recipient => current_person)
@@ -9,7 +10,7 @@ class SearchesController < ApplicationController
     end
     @results = model.constantize.search(options)
     if model == "ForumPost" and @results
-      # Cosolidate the topics, eliminating duplicates.
+      # Consolidate the topics, eliminating duplicates.
       # TODO: do this in the Topic model.  This will probably require some
       #       search-engine specific hacking, so defer to the time when we're
       #       ready to switch to Sphinx.
