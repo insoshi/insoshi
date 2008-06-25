be79c19af96073146cddb8cddabb24d633d5517b
blazar <blazar+git@gmail.com>
2008-06-25 11:56:44 -0700

    post-Piotr merge 2 - delete renumbered migrations

--- db/migrate/029_modify_person_primary_photo.rb
+++
@@ -1,9 +0,0 @@
-class ModifyPersonPrimaryPhoto < ActiveRecord::Migration
-  def self.up
-    add_column :people, :avatar_id, :integer
-  end
-
-  def self.down
-    remove_column :people, :avatar_id
-  end
-end
--- db/migrate/030_add_list_capabilityto_photos.rb
+++
@@ -1,9 +0,0 @@
-class AddListCapabilitytoPhotos < ActiveRecord::Migration
-  def self.up
-    add_column :photos, :position, :integer
-  end
-
-  def self.down
-    remove_column :photos, :position
-  end
-end
