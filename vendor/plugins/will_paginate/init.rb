853ad14724d6e4597d12b33253f63e7505c529dc
Michael Hartl <michael@michaelhartl.com>
2008-07-01 14:52:40 -0700

    Removed will_paginate plugin

--- vendor/plugins/will_paginate/init.rb
+++
@@ -1,4 +0,0 @@
-unless ActiveRecord::Base.respond_to? :paginate
-  require 'will_paginate'
-  WillPaginate.enable
-end
--- vendor/plugins/will_paginate/test/fixtures/topic.rb
+++
@@ -1,4 +0,0 @@
-class Topic < ActiveRecord::Base
-  has_many :replies, :dependent => :destroy, :order => 'replies.created_at DESC'
-  belongs_to :project
-end
