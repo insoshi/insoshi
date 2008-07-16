6c4fb31f9b21dbbf43663a33b9c52032bdc12610
Michael Hartl <michael@michaelhartl.com>
2008-07-15 20:21:42 -0700

    Switching on database adapter (and admitting that PostgreSQL isn't supported)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,14 +10,28 @@ class SearchesController < ApplicationController
     if query.blank?
       @results = [].paginate
     else
+      # We'll need to switch on the adapter name.
+      adapter = ActiveRecord::Base.connection.adapter_name
       filters = {}
       if model == "Person" and not current_person.admin?
         # Filter out deactivated and email unverified users for non-admins.
-        filters['deactivated']    = 0
-        filters['email_verified'] = 1 if global_prefs.email_verifications?
+        case adapter
+        when "MySQL"
+          filters['deactivated']    = 0
+          filters['email_verified'] = 1 if global_prefs.email_verifications?
+        when "PostgreSQL"
+          raise "PostgreSQL search is not yet supported"
+          query += " AND deactivated:f"
+          query += " AND email_verified:t" if global_prefs.email_verifications?
+        end
       elsif model == "Message"
         filters['recipient_id'] = current_person.id
-        filters['recipient_deleted_at'] = 0  # 0 is the same as NULL (!)
+        case adapter
+        when "MySQL"
+          filters['recipient_deleted_at'] = 0  # 0 is the same as NULL (!)
+        when "PostgreSQL"
+          raise "PostgreSQL search is not yet supported"
+        end
       end
       @search = Ultrasphinx::Search.new(:query => query, 
                                         :filters => filters,
