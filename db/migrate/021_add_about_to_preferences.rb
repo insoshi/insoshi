1a1252ef4b8fe332afd7e2bec75e165704e501db
blazar <blazar+git@gmail.com>
2008-07-19 20:43:20 -0700

    deleted migration files

--- db/migrate/021_add_about_to_preferences.rb
+++
@@ -1,9 +0,0 @@
-class AddAboutToPreferences < ActiveRecord::Migration
-  def self.up
-    add_column :preferences, :about, :text
-  end
-
-  def self.down
-    remove_column :preferences, :about
-  end
-end
--- db/migrate/024_add_demo_boolean.rb
+++
@@ -1,9 +0,0 @@
-class AddDemoBoolean < ActiveRecord::Migration
-  def self.up
-    add_column :preferences, :demo, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :preferences, :demo
-  end
-end
