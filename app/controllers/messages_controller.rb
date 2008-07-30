4ebfba0c8c617b6dd96c8d19691ce94a79a3d7b2
Michael Hartl <michael@michaelhartl.com>
2008-07-30 16:09:47 -0700

    Ripped out admin 'send-to-all' option See http://insoshi.lighthouseapp.com/projects/9331/tickets/102-admin-send-message-to-all-is-slow#ticket-102-2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -58,37 +58,16 @@ class MessagesController < ApplicationController
   end
 
   def create
-    
-    if params["commit"] == "Send to all" and current_person.admin?
-      # Send messages to all (active) people.
-      messages = Person.all_active.inject([]) do |messages, person|
-        message = Message.new(params[:message].merge(:sender => current_person,
-                                                     :recipient => person))
-        messages.push(message)
-      end
-      respond_to do |format|
-        @message = messages.first
-        if !preview? and @message.save
-          messages.each { |m| m.save }
-          flash[:success] = 'Message sent to everyone!'
-          format.html { redirect_to messages_url }
-        else
-          @preview = @message.content if preview?
-          format.html { render :action => "new" }
-        end
-      end
-    else
-      @message = Message.new(params[:message].merge(:sender => current_person,
-                                                    :recipient => @recipient))
-    
-      respond_to do |format|
-        if !preview? and @message.save
-          flash[:success] = 'Message sent!'
-          format.html { redirect_to messages_url }
-        else
-          @preview = @message.content if preview?
-          format.html { render :action => "new" }
-        end
+    @message = Message.new(params[:message].merge(:sender => current_person,
+                                                  :recipient => @recipient))
+  
+    respond_to do |format|
+      if !preview? and @message.save
+        flash[:success] = 'Message sent!'
+        format.html { redirect_to messages_url }
+      else
+        @preview = @message.content if preview?
+        format.html { render :action => "new" }
       end
     end
   end
