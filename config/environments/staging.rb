fa16f478447274704269df3517b61e12df09e932
Michael Hartl <michael@michaelhartl.com>
2008-07-30 13:43:48 -0700

    Fixed typo in staging environment file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,4 +18,4 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-config.log_level = :debugA
+config.log_level = :debug
