c5a5bf6c5ec53453a0cb3e4887669ea13e7835ad
Michael Hartl <michael@michaelhartl.com>
2008-07-31 14:03:39 -0700

    Removed footnotes They produce invalid HTML, which I hate. Individual users who want footnotes should install them and use the updated .gitignore to ignore them

--- vendor/plugins/footnotes/test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require 'rubygems'
-require 'mocha'
-
-ENV["RAILS_ENV"] = "test"
-
-require 'active_support'
-require File.dirname(__FILE__) + '/../lib/footnotes'
-require File.dirname(__FILE__) + '/../lib/notes/abstract_note'
