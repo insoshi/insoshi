2ee471738cd7a793fc86f8ed2100eccc44075b53
Michael Hartl <michael@michaelhartl.com>
2008-08-22 17:18:33 -0700

    Fixed a few minor style issues

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -108,7 +108,7 @@ class Connection < ActiveRecord::Base
       exist?(person, contact) and accepted?(person, contact)
     end
     
-    def pending?(person,contact)
+    def pending?(person, contact)
       exist?(person, contact) and conn(contact,person).status == PENDING
     end
   end
