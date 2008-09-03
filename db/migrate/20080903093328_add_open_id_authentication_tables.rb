b3545446b9068f63271ff04c8017387091d3b88b
Tom Brown <herestomwiththeweather@gmail.com>
2008-09-03 04:42:37 -0500

    openid migration

---
+++ db/migrate/20080903093328_add_open_id_authentication_tables.rb
@@ -0,0 +1,23 @@
+class AddOpenIdAuthenticationTables < ActiveRecord::Migration
+  def self.up
+    create_table :open_id_authentication_associations, :force => true do |t|
+      t.integer :issued, :lifetime
+      t.string :handle, :assoc_type
+      t.binary :server_url, :secret
+    end
+
+    create_table :open_id_authentication_nonces, :force => true do |t|
+      t.integer :timestamp, :null => false
+      t.string :server_url, :null => true
+      t.string :salt, :null => false
+    end
+
+    add_column :people, :identity_url, :string
+  end
+
+  def self.down
+    remove_column :people, :identity_url
+    drop_table :open_id_authentication_associations
+    drop_table :open_id_authentication_nonces
+  end
+end
