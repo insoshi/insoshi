e14a6e766c474a82ef654b2ed1cf25684fdddff8
Tom Brown <herestomwiththeweather@gmail.com>
2008-09-06 02:26:06 -0500

    bug fix: old school login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     if using_open_id?
       open_id_authentication(params[:openid_url])
     else
-      password_authentication(params[:login],params[:password])
+      password_authentication(params[:email],params[:password])
     end
   end
 
