629740a16895e9d8e57e3f30f86a4896cc829283
Michael Hartl <michael@michaelhartl.com>
2008-09-13 11:42:14 -0700

    Catching InvalidAuthenticityToken in PersonContoller#create These are almost all spam bots, and I'm tired of getting email about them from the exception notifier plugin.  But we log them, just in case.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -63,6 +63,12 @@ class PeopleController < ApplicationController
   rescue ActiveRecord::StatementInvalid
     # Handle duplicate email addresses gracefully by redirecting.
     redirect_to home_url
+  rescue ActionController::InvalidAuthenticityToken
+    # Experience has shown that the vast majority of these are bots
+    # trying to spam the system, so catch & log the exception.
+    warning = "ActionController::InvalidAuthenticityToken: #{params.inspect}"
+    logger.warn warning
+    redirect_to home_url
   end
 
   def verify_email
