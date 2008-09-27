de8311fb71dc9ab50b239c9050ba8751cc2473e9
Tom Brown <herestomwiththeweather@gmail.com>
2008-09-27 17:42:16 -0500

    removed connect to admin callback so it doesn't get logged

--- app/models/person.rb
+++ app/models/person.rb
@@ -113,7 +113,7 @@ class Person < ActiveRecord::Base
   before_create :create_blog, :check_config_for_deactivation
   before_save :encrypt_password
   before_validation :prepare_email, :handle_nil_description
-  after_create :connect_to_admin
+  #after_create :connect_to_admin
 
   before_update :set_old_description
   after_update :log_activity_description_changed
