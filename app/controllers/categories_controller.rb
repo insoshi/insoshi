e30990dc2af239da1266d8bb8d9cf24f9065fc8b
Tom Brown <herestomwiththeweather@gmail.com>
2008-09-27 18:07:33 -0500

    deleted categories layout. required login for various actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApplicationController
+
+  before_filter :login_required, :only => [ :show, :edit, :update ]
+
   # GET /categories
   # GET /categories.xml
   def index
