ffa6a82b554dc622eb13d04a7973d07c7ea5a5ef
Tom Brown <herestomwiththeweather@gmail.com>
2008-09-30 01:33:53 -0500

    using att_protected instead of attr_accessible for Req. ugh.  attr_accessible did not seem to work with habtm categories

--- app/models/req.rb
+++ app/models/req.rb
@@ -4,7 +4,7 @@ class Req < ActiveRecord::Base
   has_and_belongs_to_many :categories
   belongs_to :person
 
-  attr_accessible :name, :description, :estimated_hours, :due_date
+  attr_protected :person_id
 
   after_create :log_activity
 
