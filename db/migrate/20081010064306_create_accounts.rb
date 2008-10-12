38ab72e37f176a027dcecc462c0d6ce5538cb0aa
Tom Brown <herestomwiththeweather@gmail.com>
2008-10-12 14:36:01 -0500

    change precision and scale on decimals

--- db/migrate/20081010064306_create_accounts.rb
+++ db/migrate/20081010064306_create_accounts.rb
@@ -2,7 +2,7 @@ class CreateAccounts < ActiveRecord::Migration
   def self.up
     create_table :accounts do |t|
       t.string :name
-      t.decimal :balance
+      t.decimal :balance, :precision => 8, :scale => 2, :default => 0
       t.integer :person_id
 
       t.timestamps
