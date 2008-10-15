25837070218a1b74abe0b873f6f8dbdfb2ae5478
Michael Hartl <michael@michaelhartl.com>
2008-10-14 19:11:00 -0700

    Rejiggered

--- db/migrate/20081015002258_migrate_photo_data.rb
+++ db/migrate/20081015002258_migrate_photo_data.rb
@@ -4,7 +4,7 @@ class MigratePhotoData < ActiveRecord::Migration
     # For each person, create a gallery and put all their photos there.
     Person.find(:all).each do |person|
       gallery = person.galleries.new
-      gallery.title = "Primary gallery"
+      gallery.title = "Primary"
       primary_photo = person.photos.detect(&:primary?)
       gallery.primary_photo_id = primary_photo.id unless primary_photo.nil?
       Gallery.skip_callback(:log_activity) do
@@ -21,15 +21,15 @@ class MigratePhotoData < ActiveRecord::Migration
   end
 
   def self.down
-    Gallery.find(:all).each do |gallery|
-      gallery.destroy
-    end
     Photo.find(:all).each do |photo|
       Photo.skip_callback(:log_activity) do
         photo.gallery_id = nil
         photo.primary = photo.avatar
         photo.save(false)
       end
+    end
+    Gallery.find(:all).each do |gallery|
+      gallery.destroy
     end
   end
 end
