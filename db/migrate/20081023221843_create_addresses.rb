fe5d3052e76b3085e7f1db25536a94ee78f9f783
Tom Brown <herestomwiththeweather@gmail.com>
2008-10-23 17:26:54 -0500

    address model.  person has many addresses

---
+++ db/migrate/20081023221843_create_addresses.rb
@@ -0,0 +1,22 @@
+class CreateAddresses < ActiveRecord::Migration
+  def self.up
+    create_table :addresses do |t|
+        t.integer :person_id
+        t.string :name, :null => true, :limit => 50
+        t.string :address_line_1, :null => true, :limit => 50
+        t.string :address_line_2, :null => true, :limit => 50
+        t.string :address_line_3, :null => true, :limit => 50
+        t.string :city, :null => true, :limit => 50
+        t.string :county_id, :null => true
+        t.integer :state_id, :null => true
+        t.string :zipcode_plus_4, :null => true, :limit => 10
+        t.decimal :latitude, :null => false, :precision => 12, :scale => 8
+        t.decimal :longitude, :null => false, :precision => 12, :scale => 8
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :addresses
+  end
+end
