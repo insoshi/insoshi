8f3d4ddd11e084709107eb5271fbd47d40c29ed5
Michael Hartl <michael@michaelhartl.com>
2008-10-28 10:28:10 -0700

    Fixed session#create being hit by a bot

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,10 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Protect against bots hitting us.
+    if params[:email].nil? or params[:password].nil?
+      render :text => "" and return
+    end
     if using_open_id?
       open_id_authentication(params[:openid_url])
     else
