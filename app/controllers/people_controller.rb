757bf03d90be14fd393d457ec20455700a5fc751
Michael Hartl <michael@michaelhartl.com>
2008-10-28 10:29:07 -0700

    Put common contacts behind login wall (bots were hitting it and raising exceptions)

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,8 @@ class PeopleController < ApplicationController
   
   skip_before_filter :require_activation, :only => :verify_email
   skip_before_filter :admin_warning, :only => [ :show, :update ]
-  before_filter :login_required, :only => [ :show, :edit, :update ]
+  before_filter :login_required, :only => [ :show, :edit, :update,
+                                            :common_contacts ]
   before_filter :correct_user_required, :only => [ :edit, :update ]
   before_filter :setup
   
