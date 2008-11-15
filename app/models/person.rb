ab04cbbd67d75b7bc58e2eb3d4493fc4ef6af57e
Tom Brown <herestomwiththeweather@gmail.com>
2008-11-15 15:49:44 -0600

    fixed regression on messages from active scaffold admin

--- app/models/person.rb
+++ app/models/person.rb
@@ -76,13 +76,13 @@ class Person < ActiveRecord::Base
   has_many :requested_contacts, :through => :connections,
            :source => :contact
            #:conditions => REQUESTED_AND_ACTIVE
-  #with_options :class_name => "Message", :dependent => :destroy,
-  #             :order => 'created_at DESC' do |person|
-  #  person.has_many :_sent_messages, :foreign_key => "sender_id",
-  #                  :conditions => "sender_deleted_at IS NULL"
-  #  person.has_many :_received_messages, :foreign_key => "recipient_id",
-  #                  :conditions => "recipient_deleted_at IS NULL"
-  #end
+  with_options :class_name => "Message", :dependent => :destroy,
+               :order => 'created_at DESC' do |person|
+    person.has_many :_sent_messages, :foreign_key => "sender_id",
+                    :conditions => "sender_deleted_at IS NULL"
+    person.has_many :_received_messages, :foreign_key => "recipient_id",
+                    :conditions => "recipient_deleted_at IS NULL"
+  end
   has_many :feeds
   has_many :activities, :through => :feeds, :order => 'activities.created_at DESC',
                                             :limit => FEED_SIZE,
