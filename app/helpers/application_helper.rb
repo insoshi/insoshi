6fa1b6390abe0b5ed2bcced1c64b1d41bc42cfea
Rich Vazquez <rich.vazquez@gmail.com>
2008-11-19 23:07:15 -0600

    remove image button, begin yui styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -214,11 +214,58 @@ def yui_rte(textspace)
          content_for(:yui_rte) {'           
            <script type="text/javascript">
            var myEditor = new YAHOO.widget.Editor("' + textspace + '", {
-               height: "300px",
                handleSubmit: true,
-               width: "522px",
-               dompath: true, //Turns on the bar at the bottom
-               animate: true //Animates the opening, closing and moving of Editor windows
+               dompath: false, //Turns on the bar at the bottom
+               animate: true, //Animates the opening, closing and moving of Editor windows
+               collapse: true,
+               titlebar: "This is text",
+               draggable: false,
+               buttons: [
+                         { group: "fontstyle", label: "Font Name and Size",
+                             buttons: [
+                                 { type: "select", label: "Arial", value: "fontname", disabled: true,
+                                     menu: [
+                                         { text: "Arial", checked: true },
+                                         { text: "Arial Black" },
+                                         { text: "Comic Sans MS" },
+                                         { text: "Courier New" },
+                                         { text: "Lucida Console" },
+                                         { text: "Tahoma" },
+                                         { text: "Times New Roman" },
+                                         { text: "Trebuchet MS" },
+                                         { text: "Verdana" }
+                             ]
+                           },
+                           { type: "spin", label: "13", value: "fontsize", range: [ 9, 75 ], disabled: true }
+                       ]
+                   },
+                   { type: "separator" },
+                   { group: "textstyle", label: "Font Style",
+                       buttons: [
+                           { type: "push", label: "Bold CTRL + SHIFT + B", value: "bold" },
+                           { type: "push", label: "Italic CTRL + SHIFT + I", value: "italic" },
+                           { type: "push", label: "Underline CTRL + SHIFT + U", value: "underline" },
+                           { type: "separator" },
+                           { type: "color", label: "Font Color", value: "forecolor", disabled: true },
+                           { type: "color", label: "Background Color", value: "backcolor", disabled: true }
+                       ]
+                   },
+                   { type: "separator" },
+                   { group: "indentlist", label: "Lists",
+                       buttons: [
+                           { type: "push", label: "Create an Unordered List", value: "insertunorderedlist" },
+                           { type: "push", label: "Create an Ordered List", value: "insertorderedlist" }
+                       ]
+                   },
+                   { type: "separator" },
+                   { group: "insertitem", label: "Insert Item",
+                       buttons: [
+                           { type: "push", label: "HTML Link CTRL + SHIFT + L", value: "createlink", disabled: true },
+                           { type: "push", label: "Insert Image", value: "insertimage" }
+                       ]
+                   }
+               ]
+               
            });
            myEditor.render();
            </script>
