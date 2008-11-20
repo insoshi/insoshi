79f8a28ddf00ecfeeb20bd063f47908180e9c6cd
Rich Vazquez <rich.vazquez@gmail.com>
2008-11-19 20:02:38 -0600

    restore a person body class

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -98,7 +98,7 @@ class PeopleController < ApplicationController
   end
 
   def edit
-    @body = "yui-skin-sam"
+    @body = @body + " yui-skin-sam"
     @person = Person.find(params[:id])
     @all_categories = Category.find(:all, :order => "parent_id, name").sort_by { |a| a.long_name }
 
