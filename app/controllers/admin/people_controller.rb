640ff3153bb2ab4737b7ac55488a3ef71534ad51
Tom Brown <herestomwiththeweather@gmail.com>
2008-11-20 01:07:28 -0600

    new phone field in people is only accessible through active scaffold. explicitly set column order per action due to active scaffold bug.  removed deactivated field from create but keep it in update

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -2,8 +2,10 @@ class Admin::PeopleController < ApplicationController
   before_filter :login_required, :admin_required
   active_scaffold :person do |config|
     config.label = "People"
-    config.columns = [:name, :email, :admin, :deactivated, :email_verified, :last_logged_in_at]
-    config.create.columns = [:name, :email, :password, :password_confirmation, :admin, :deactivated] 
+    config.columns = [:name, :email, :phone, :admin, :deactivated, :email_verified, :last_logged_in_at]
+    config.list.columns = [:name, :email, :phone, :admin, :deactivated, :email_verified, :last_logged_in_at]
+    config.create.columns = [:name, :email, :phone, :password, :password_confirmation, :admin] 
+    config.update.columns = [:name, :email, :phone, :password, :password_confirmation, :admin, :deactivated] 
     config.nested.add_link('Addresses', [:addresses])
   end
 end
