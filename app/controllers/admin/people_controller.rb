99b1ec278e0d1af2ba9457e15750d81297701ac2
Tom Brown <herestomwiththeweather@gmail.com>
2008-11-20 19:18:03 -0600

    new people created through active scaffold have email_verified set to true. removed unnecessary accordion styling

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -4,8 +4,14 @@ class Admin::PeopleController < ApplicationController
     config.label = "People"
     config.columns = [:name, :email, :phone, :admin, :deactivated, :email_verified, :last_logged_in_at]
     config.list.columns = [:name, :email, :phone, :admin, :deactivated, :email_verified, :last_logged_in_at]
-    config.create.columns = [:name, :email, :phone, :password, :password_confirmation, :admin] 
-    config.update.columns = [:name, :email, :phone, :password, :password_confirmation, :admin, :deactivated] 
+    config.create.columns = [:name, :email, :phone, :password, :password_confirmation] 
+    config.update.columns = [:name, :email, :phone, :password, :password_confirmation, :deactivated, :email_verified] 
     config.nested.add_link('Addresses', [:addresses])
+  end
+
+  protected
+
+  def before_create_save(record)
+    record.email_verified = true
   end
 end
