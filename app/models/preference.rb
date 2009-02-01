8cd6aadae65d4dc011a7a7856e27867fd4e71719
Tom Brown <herestomwiththeweather@gmail.com>
2009-02-01 02:20:18 -0600

    admin preferences for twitter creds

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -18,21 +18,38 @@
 #
 
 class Preference < ActiveRecord::Base
+  attr_accessor :twitter_password
   attr_accessible :app_name, :server_name, :domain, :smtp_server, 
                   :exception_notification,
                   :email_notifications, :email_verifications, :analytics,
                   :about, :demo, :whitelist, :gmail, :registration_notification,
-                  :practice, :steps, :questions, :memberships, :contact
+                  :practice, :steps, :questions, :memberships, :contact,
+                  :twitter_name, :twitter_password
 
   validates_presence_of :domain,       :if => :using_email?
   validates_presence_of :smtp_server,  :if => :using_email?
   
+  before_save :encrypt_twitter_password
+
   # Can we send mail with the present configuration?
   def can_send_email?
     not (domain.blank? or smtp_server.blank?)
   end
   
   private
+    def self.encrypt(password)
+      Crypto::Key.from_file("#{RAILS_ROOT}/rsa_key.pub").encrypt(password)
+    end
+  
+    # Encrypts the password with the user salt
+    def encrypt(password)
+      self.class.encrypt(password)
+    end
+
+    def encrypt_twitter_password
+      return if twitter_password.blank?
+      self.crypted_twitter_password = encrypt(twitter_password)
+    end
   
     def using_email?
       email_notifications? or email_verifications?
