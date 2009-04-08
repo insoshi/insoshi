0ddf8f9ee2677ac43ff109387279bf2dc10d8f8d
Tom Brown <herestomwiththeweather@gmail.com>
2009-04-08 17:09:34 -0500

    ajax display people to be notified of new request because of category match.  need to remove duplicate images

--- public/javascripts/req_form.js
+++ public/javascripts/req_form.js
@@ -2,9 +2,24 @@
  *
  */
 
-jQuery(function($) {
+jQuery(function($) { 
+
+  function processPerson( n, peep ) {
+    var previous_node_id = ( n == 0 ) ? "#followMe" : "#peep" + (n-1);
+    if( "default_icon.png" == peep.icon )
+    {
+      peep.icon = "/images/" + peep.icon;
+    }
+    $("<div id=\"peep" + n + "\" style=\"float: left;\"></div>").insertAfter(previous_node_id);
+    $('#peep' + n).html("<img src=\"" + peep.icon + "\" title=\"" + peep.name + "\"</>");
+  }
+
   $("#req_due_date").datepicker({
 buttonImage: "/images/calendar.gif",
 buttonImageOnly: true
     });
+  $("#req_category_ids").change( 
+    function(){ var category_id = $(this).val();
+                $.getJSON( '/categories/' + category_id, '', function(data){peeps = data.category.people;$.each(peeps,processPerson);});
+              });
 });
