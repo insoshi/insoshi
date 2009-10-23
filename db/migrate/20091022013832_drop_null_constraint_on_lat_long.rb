e603ec71e98215cc696dcbc626f705611175b1ff
Tom Brown <herestomwiththeweather@gmail.com>
2009-10-23 05:51:59 -0500

    using change_column instead of sql to remove not null constraint.  reverting create_addresses to original state for posterity sake

--- db/migrate/20091022013832_drop_null_constraint_on_lat_long.rb
+++ db/migrate/20091022013832_drop_null_constraint_on_lat_long.rb
@@ -1,14 +1,9 @@
 class DropNullConstraintOnLatLong < ActiveRecord::Migration
   def self.up
-    execute("ALTER TABLE addresses ALTER COLUMN latitude DROP NOT NULL")
-    execute("ALTER TABLE addresses ALTER COLUMN longitude DROP NOT NULL")
+    change_column :addresses, :latitude, :decimal, :null => :true, :precision => 12, :scale => 8
+    change_column :addresses, :longitude, :decimal, :null => :true, :precision => 12, :scale => 8
   end
 
   def self.down
-    puts "THIS WILL ERASE ALL YOUR LOCATIONS."
-    execute("UPDATE addresses SET longitude = 0 WHERE longitude IS NULL")
-    execute("UPDATE addresses SET latitude = 0 WHERE latitude IS NULL")
-    execute("ALTER TABLE addresses ALTER COLUMN latitude SET NOT NULL")
-    execute("ALTER TABLE addresses ALTER COLUMN longitude SET NOT NULL")
   end
 end
