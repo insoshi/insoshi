161ebbcf4cee9a719f0bd3cf043a145aec06ed70
Tom Brown <herestomwiththeweather@gmail.com>
2010-01-21 13:00:03 -0600

    heroku master: restore consider_all_requests_local to false for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ config.cache_classes = false
 config.whiny_nils = true
 
 # Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
+config.action_controller.consider_all_requests_local = false
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
