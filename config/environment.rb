60cec8c8d8ae3d3ed6925d4f1a3f9de6aeb8833b
Tom Brown <herestomwiththeweather@gmail.com>
2010-02-02 00:43:29 -0600

    don't require secret_key_generator

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,6 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require 'rails_generator/secret_key_generator'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
