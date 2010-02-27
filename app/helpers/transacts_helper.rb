2bd6d8d356b3fb79d26c81f6a231d3aa28ddca0e
Tom Brown <herestomwiththeweather@gmail.com>
2010-02-27 16:55:40 -0600

    internationalize opentransact views, helper

--- app/helpers/transacts_helper.rb
+++ app/helpers/transacts_helper.rb
@@ -4,11 +4,11 @@ module TransactsHelper
   end
 
   def counterparty_action(t)
-    units = t.group.nil? ? "hours" : t.group.unit
+    units = t.group.nil? ? t('currency_unit_plural') : t.group.unit
     if current_person?(t.worker)
-      "+#{t.amount.to_s} #{units} (received from "
+      "+#{t.amount.to_s} #{units} (" + t('transacts.helper.received_from') + " "
     else
-      "-#{t.amount.to_s} #{units} (paid to "
+      "-#{t.amount.to_s} #{units} (" + t('transacts.helper.paid_to') + " "
     end
   end
 end
