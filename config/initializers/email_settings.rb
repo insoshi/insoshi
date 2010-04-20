2d4c675ee3bd3dd3a4b6cb3fe5435f7fa468ee84
Tom Brown <herestomwiththeweather@gmail.com>
2010-04-20 17:43:09 -0500

    Polushkin fix for ActionMailer

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -1,3 +1,21 @@
+
+module ActionMailer
+  class Base
+    def perform_delivery_smtp(mail)
+      destinations = mail.destinations
+      mail.ready_to_send
+      sender = (mail['return-path'] && mail['return-path'].spec) || Array(mail.from).first
+
+      smtp = Net::SMTP.new(smtp_settings[:address], smtp_settings[:port])
+      smtp.enable_starttls_auto if smtp_settings[:enable_starttls_auto] && smtp.respond_to?(:enable_starttls_auto)
+      smtp.start(smtp_settings[:domain], smtp_settings[:user_name], smtp_settings[:password],
+                 smtp_settings[:authentication]) do |smtp|
+        smtp.sendmail(mail.encoded, sender, destinations)
+      end
+    end
+  end
+end
+
 begin
   unless test?
     global_prefs = Preference.find(:first)
