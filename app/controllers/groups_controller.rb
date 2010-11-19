insoshi/insoshi

commit b8180792018ad4919c878434f2ab62f59fe24dcb

groupy branch: specs and cancan abilities for group roles and exchanges. fetch account by group.

app/controllers/groups_controller.rb
@@ -1,11 +1,29 @@
 class GroupsController < ApplicationController
+  load_and_authorize_resource
   skip_before_filter :require_activation
   before_filter :login_or_oauth_required
-  before_filter :group_owner, :only => [:edit, :update, :destroy, 
-    :new_photo, :save_photo, :delete_photo]
   
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    respond_to do |format|
+      format.html {redirect_to @group}
+      format.js do
+        canvas = case params[:action] 
+          when 'new_req','create_req'
+            'reqs_canvas'
+          when 'new_offer','create_offer'
+            'offers_canvas'
+          else
+            'empty_canvas'
+          end
+        render :partial => 'flash_messages', :locals => {:canvas_id => canvas}
+      end
+    end
+  end
+
   def index
-    @groups = Group.not_hidden(params[:page])
+    # XXX can't define abilities w/ blocks (accessible_by) http://github.com/ryanb/cancan/wiki/Upgrading-to-1.4
+    @groups = @groups.not_hidden(params[:page])
 
     respond_to do |format|
       format.html
@@ -13,7 +31,6 @@ class GroupsController < ApplicationController
   end
 
   def new_req
-    @group = Group.find(params[:id])
     @req = Req.new
     @all_categories = Category.all
 
@@ -23,7 +40,6 @@ class GroupsController < ApplicationController
   end
 
   def create_req
-    @group = Group.find(params[:id])
     @req = Req.new(params[:req])
     @req.group = @group
 
@@ -37,6 +53,7 @@ class GroupsController < ApplicationController
     respond_to do |format|
       if @req.save
         flash[:notice] = 'Request was successfully created.'
+        @reqs = @group.reqs.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
         format.js
       else
         @all_categories = Category.all
@@ -46,7 +63,6 @@ class GroupsController < ApplicationController
   end
 
   def new_offer
-    @group = Group.find(params[:id])
     @offer = Offer.new
     @all_categories = Category.all
 
@@ -56,7 +72,6 @@ class GroupsController < ApplicationController
   end
 
   def create_offer
-    @group = Group.find(params[:id])
     @offer = Offer.new(params[:offer])
     @offer.group = @group
     ##TODO: move this to the model, a before_create method?
@@ -66,6 +81,7 @@ class GroupsController < ApplicationController
     respond_to do |format|
       if @offer.save
         flash[:notice] = 'Offer was successfully created.'
+        @offers = @group.offers.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
         format.js
       else
         @all_categories = Category.all
@@ -79,7 +95,6 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @group = Group.find(params[:id])
     @forum = @group.forum
     @topics = Topic.find_recently_active(@forum, params[:page]) 
     @contacts = contacts_to_invite
@@ -97,19 +112,15 @@ class GroupsController < ApplicationController
   end
 
   def new
-    @group = Group.new
-
     respond_to do |format|
       format.html
     end
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def create
-    @group = Group.new(params[:group])
     @group.owner = current_person
 
     respond_to do |format|
@@ -125,8 +136,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
-
     respond_to do |format|
       if @group.update_attributes(params[:group])
         flash[:notice] = t('notice_group_updated')
@@ -138,7 +147,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
 
     respond_to do |format|
@@ -148,7 +156,6 @@ class GroupsController < ApplicationController
   end
   
   def invite
-    @group = Group.find(params[:id])
     @contacts = contacts_to_invite
 
     respond_to do |format|
@@ -165,7 +172,6 @@ class GroupsController < ApplicationController
   end
   
   def invite_them
-    @group = Group.find(params[:id])
     invitations = params[:checkbox].collect{|x| x if  x[1]=="1" }.compact
     invitations.each do |invitation|
       if Membership.find_all_by_group_id(@group, :conditions => ['person_id = ?',invitation[0].to_i]).empty?
@@ -179,7 +185,6 @@ class GroupsController < ApplicationController
   end
   
   def members
-    @group = Group.find(params[:id])
     @members = @group.people.paginate(:page => params[:page],
                                           :per_page => RASTER_PER_PAGE)
     @pending = @group.pending_request.paginate(:page => params[:page],
@@ -188,7 +193,7 @@ class GroupsController < ApplicationController
   end
 
   def photos
-    @group = Group.find(params[:id])
+    #@group = Group.find(params[:id])
     @photos = @group.photos
     respond_to do |format|
       format.html
@@ -196,36 +201,34 @@ class GroupsController < ApplicationController
   end
   
   def new_photo
-    @photo = Photo.new
-
     respond_to do |format|
       format.html
     end
   end
   
   def save_photo
-    group = Group.find(params[:id])
+    #group = Group.find(params[:id])
     if params[:photo].nil?
       # This is mainly to prevent exceptions on iPhones.
       flash[:error] = t('error_browser_upload_fail')
-      redirect_to(edit_group_path(group)) and return
+      redirect_to(edit_group_path(@group)) and return
     end
     if params[:commit] == "Cancel"
       flash[:notice] = t('notice_upload_canceled')
-      redirect_to(edit_group_path(group)) and return
+      redirect_to(edit_group_path(@group)) and return
     end
     
-    group_data = { :group => group,
-                    :primary => group.photos.empty? }
+    group_data = { :group => @group,
+                    :primary => @group.photos.empty? }
     @photo = Photo.new(params[:photo].merge(group_data))
     
     respond_to do |format|
       if @photo.save
         flash[:success] = t('success_photo_uploaded')
-        if group.owner == current_person
-          format.html { redirect_to(edit_group_path(group)) }
+        if @group.owner == current_person
+          format.html { redirect_to(edit_group_path(@group)) }
         else
-          format.html { redirect_to(group_path(group)) }
+          format.html { redirect_to(group_path(@group)) }
         end
       else
         format.html { render :action => "new_photo" }

.gems
@@ -12,3 +12,4 @@ rack-openid
 aws-s3
 heroku
 rmagick
+cancan

app/controllers/exchanges_controller.rb
@@ -1,10 +1,12 @@
 class ExchangesController < ApplicationController
+  load_resource :person
+  load_and_authorize_resource :exchange, :through => :person
   skip_before_filter :require_activation
   before_filter :login_or_oauth_required
   before_filter :find_worker
 
   def index
-    @exchanges = @worker.received_exchanges # created_at DESC
+    #@exchanges = @worker.received_exchanges # created_at DESC
     respond_to do |format|
       format.xml { render :xml => @exchanges }
       format.json { render :json => @exchanges.as_json( :include => :req ) }
@@ -12,7 +14,6 @@ class ExchangesController < ApplicationController
   end
 
   def show
-    @exchange = Exchange.find(params[:id])
     respond_to do |format|
       format.xml { render :xml => @exchange.to_xml( :include => :req ) }
       format.json { render :json => @exchange.as_json( :include => :req ) }
@@ -20,8 +21,6 @@ class ExchangesController < ApplicationController
   end
 
   def new
-    @exchange = Exchange.new
-
     if params[:offer]
       @offer = Offer.find(params[:offer])
       if @offer.person != Person.find(params[:person_id])
@@ -45,7 +44,7 @@ class ExchangesController < ApplicationController
   #
   def create
     @exchange = Exchange.new(params[:exchange]) # amount and group_id are the only accessible fields
-    @exchange.worker = @worker
+    @exchange.worker = @person
     @exchange.customer = current_person
 
     if params[:offer]
@@ -57,6 +56,7 @@ class ExchangesController < ApplicationController
       @req = Req.new(params[:req])
 
       @req.name = 'Gift transfer' if @req.name.blank? # XML creation might not set this
+      @req.group = @exchange.group
       @req.estimated_hours = @exchange.amount
       @req.due_date = Time.now
       @req.person = current_person
@@ -69,10 +69,11 @@ class ExchangesController < ApplicationController
 
     respond_to do |format|
       if @exchange.save
+        logger.info "XXX exchange succeeded!"
         flash[:notice] = t('success_credit_transfer_succeeded')
-        format.html { redirect_to person_path(@worker) and return }
-        format.xml { render :xml => @exchange, :status => :created, :location => [@worker, @exchange] }
-        format.json { render :json => @exchange, :status => :created, :location => [@worker, @exchange] }
+        format.html { redirect_to person_path(@person) and return }
+        format.xml { render :xml => @exchange, :status => :created, :location => [@person, @exchange] }
+        format.json { render :json => @exchange, :status => :created, :location => [@person, @exchange] }
         format.js
       else
         flash[:error] = t('error_with_credit_transfer')
@@ -86,18 +87,17 @@ class ExchangesController < ApplicationController
   end
 
   def destroy
-    @exchange = Exchange.find(params[:id])
     @metadata = @exchange.metadata
 
     begin
       Exchange.transaction do
-        @worker.account.withdraw(@exchange.amount)
-        current_person.account.deposit(@exchange.amount)
+        @person.account(@exchange.group).withdraw(@exchange.amount)
+        current_person.account(@exchange.group).deposit(@exchange.amount)
       end
     rescue
       respond_to do |format|
         flash[:error] = t('error_with_suspension')
-        format.html { redirect_to person_path(@worker) and return }
+        format.html { redirect_to person_path(@person) and return }
       end
     end
 
@@ -114,10 +114,4 @@ class ExchangesController < ApplicationController
     end
   end
 
-private
-  def find_worker
-    @worker_id = params[:person_id]
-    redirect_to home_url and return unless @worker_id
-    @worker = Person.find(@worker_id)
-  end
 end

app/controllers/transacts_controller.rb
@@ -54,7 +54,8 @@ class TransactsController < ApplicationController
 
     @transact.metadata = @transact.create_req(params[:memo])
 
-    if @transact.save
+    # XXX this will fail.
+    if can?(:create, @transact) && @transact.save
       if @transact.redirect_url.blank?
         flash[:notice] = t('notice_transfer_succeeded')
         respond_to do |format|
@@ -68,7 +69,7 @@ class TransactsController < ApplicationController
       end
     else
       flash[:error] = t('error_with_transfer')
-      @req.destroy
+      @transact.metadata.destroy
       render :action => "new"
     end
 

app/models/ability.rb
@@ -1,0 +1,30 @@
+class Ability
+  include CanCan::Ability
+  def initialize(person)
+    can [:read, :show_default, :create], Group
+    can [:new_req,:create_req], Group
+    can [:new_offer,:create_offer], Group
+    can [:update,:destroy], Group, :owner => person
+    can :update, Group do |group|
+      membership = Membership.mem(person,group)
+      membership && membership.is?(:admin)
+    end
+    can [:new_photo,:save_photo,:delete_photo], Group, :owner => person
+
+    can :read, Exchange
+    can :destroy, Exchange, :customer => person
+    can :create, Exchange do |exchange|
+      unless exchange.group
+        # all exchanges are associated with a group
+        false
+      else
+        membership = Membership.mem(person,exchange.group)
+        unless membership
+          false
+        else
+          true
+        end
+      end
+    end
+  end
+end

app/models/account.rb
@@ -33,8 +33,8 @@ class Account < ActiveRecord::Base
   def self.transfer(from, to, amount, metadata)
     transaction do
       exchange = Exchange.new()
-      exchange.customer = from.person
-      exchange.worker = to.person
+      exchange.customer = from
+      exchange.worker = to
       exchange.amount = amount
       exchange.metadata = metadata
       exchange.group_id = metadata.group_id

app/models/bid.rb
@@ -164,6 +164,7 @@ class Bid < ActiveRecord::Base
   def trigger_approved
     self.approved_at = Time.now
     save
-    Account.transfer(self.req.person.account,self.person.account,self.estimated_hours,self.req)
+
+    Account.transfer(self.req.person, self.person, self.estimated_hours, self.req)
   end
 end

app/models/exchange.rb
@@ -42,6 +42,15 @@ class Exchange < ActiveRecord::Base
     Exchange.sum(:amount, :conditions => ["date(created_at) = ?", date])
   end
 
+  # XXX person_id hacks for cancan's load_and_authorize_resource
+  def person_id
+    self.worker_id
+  end
+
+  def person_id=(worker_id)
+    self.worker_id = worker_id
+  end
+
   def self.total_on_month(date)
     Exchange.sum(:amount, :conditions => ["DATE_TRUNC('month',created_at) = ?", date])
   end
@@ -85,11 +94,11 @@ class Exchange < ActiveRecord::Base
     begin
       Account.transaction do
         if group.nil?
-          worker.account.deposit(amount)
-          customer.account.withdraw(amount)
+          # this should not happen anymore
+          raise "no group specified"
         else
-          worker.accounts.find(:first, :conditions => ["group_id = ?",group.id]).deposit(amount)
-          customer.accounts.find(:first, :conditions => ["group_id = ?",group.id]).withdraw(amount)
+          worker.account(group).deposit(amount)
+          customer.account(group).withdraw(amount)
         end
       end
     rescue
@@ -102,6 +111,8 @@ class Exchange < ActiveRecord::Base
   end
 
   def send_payment_notification_to_worker
+    # XXX fix wierdness related to cancan
+=begin
     exchange_note = Message.new()
     subject = "PAYMENT: " + self.amount.to_s + " hours - for " + self.metadata.name 
     exchange_note.subject =  subject.length > 75 ? subject.slice(0,75).concat("...") : subject 
@@ -109,6 +120,7 @@ class Exchange < ActiveRecord::Base
     exchange_note.sender = self.customer
     exchange_note.recipient = self.worker
     exchange_note.save!
+=end
   end
 
   def send_suspend_payment_notification_to_worker

commit ae598b0e47257546af883e9c99569ab391191dbc

groupy branch: fix regression introduced in b8180792018ad4919c878434f2ab62f59fe24dcb

app/controllers/groups_controller.rb
@@ -92,6 +92,7 @@ class GroupsController < ApplicationController
     respond_to do |format|
       format.html
       format.xml { render :xml => @group.to_xml(:methods => [:icon,:thumbnail], :only => [:id,:name,:description,:mode,:person_id,:created_at,:updated_at,:unit,:icon,:thumbnail]) }
+      format.js
     end
 
   end