insoshi/insoshi

commit 0b0213464086bf5a005ed460e963f74ee71563f6

groupy branch: get simple per-group payment working via ajax ui

app/controllers/groups_controller.rb
@@ -14,7 +14,7 @@ class GroupsController < ApplicationController
 
   def new_req
     @req = Req.new
-    @all_categories = Category.all
+    @all_categories = Category.find(:all, :order => "parent_id, name").sort_by { |a| a.long_name }
 
     respond_to do |format|
       format.js
@@ -46,7 +46,7 @@ class GroupsController < ApplicationController
 
   def new_offer
     @offer = Offer.new
-    @all_categories = Category.all
+    @all_categories = Category.find(:all, :order => "parent_id, name").sort_by { |a| a.long_name }
 
     respond_to do |format|
       format.js
@@ -77,23 +77,35 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @forum = @group.forum
-    @topics = Topic.find_recently_active(@forum, params[:page]) 
-    @reqs = @group.reqs.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
-    @offers = @group.offers.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
-    @exchanges = @group.exchanges.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
-    @memberships = @group.memberships.active.paginate(:page => params[:page],
-                                          :conditions => ['status = ?', Membership::ACCEPTED],
-                                          :order => 'memberships.created_at DESC',
-                                          :include => :person,
-                                          :per_page => AJAX_POSTS_PER_PAGE)
-    if Membership.exists?(current_person,@group)
-      @add_membership_display = 'hide'
-      @membership_display = ''
+    case params[:tab]
+    when 'forum'
+      @forum = @group.forum
+    when 'requests'
+      @reqs = @group.reqs.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
+    when 'offers'
+      @offers = @group.offers.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
+    when 'exchanges'
+      @exchanges = @group.exchanges.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
+    when 'people'
+      @memberships = @group.memberships.active.paginate(:page => params[:page],
+                                            :conditions => ['status = ?', Membership::ACCEPTED],
+                                            :order => 'memberships.created_at DESC',
+                                            :include => :person,
+                                            :per_page => AJAX_POSTS_PER_PAGE)
     else
-      @add_membership_display = ''
-      @membership_display = 'hide'
+      @forum = @group.forum
+      @topics = Topic.find_recently_active(@forum, params[:page]) 
+      @reqs = @group.reqs.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
+      @offers = @group.offers.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
+      @exchanges = @group.exchanges.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
+      @memberships = @group.memberships.active.paginate(:page => params[:page],
+                                            :conditions => ['status = ?', Membership::ACCEPTED],
+                                            :order => 'memberships.created_at DESC',
+                                            :include => :person,
+                                            :per_page => AJAX_POSTS_PER_PAGE)
+      membership_display(current_person,@group)
     end
+
     respond_to do |format|
       format.html
       format.xml { render :xml => @group.to_xml(:methods => [:icon,:thumbnail], :only => [:id,:name,:description,:mode,:person_id,:created_at,:updated_at,:unit,:icon,:thumbnail]) }
@@ -213,4 +225,15 @@ class GroupsController < ApplicationController
     end
   end
   
+  protected
+
+  def membership_display(p,g)
+    if Membership.exists?(p,g)
+      @add_membership_display = 'hide'
+      @membership_display = ''
+    else
+      @add_membership_display = ''
+      @membership_display = 'hide'
+    end
+  end
 end

public/javascripts/group.js
@@ -123,13 +123,7 @@ $(function() {
     $.getScript(document.location.href+'?tab=people');
     });
 
-  $('.req a.show-follow').live('click',function() {
-    $('span.wait').show();
-    $.getScript(this.href);
-    return false;
-    });
-
-  $('.offer a.show-follow').live('click',function() {
+  $('a.show-follow').live('click',function() {
     $('span.wait').show();
     $.getScript(this.href);
     return false;

app/controllers/exchanges_controller.rb
@@ -29,15 +29,19 @@ class ExchangesController < ApplicationController
   def new
     if params[:offer]
       @offer = Offer.find(params[:offer])
-      if @offer.person != Person.find(params[:person_id])
+      if @offer.person != @person
         flash[:error] = t('error_offer_person_mismatch')
       end
     else
+      if params[:group]
+        @group = Group.find(params[:group])
+      end
       @req = Req.new
       @req.name = 'Enter description of service here'
     end
 
-    @groups = Person.find(params[:person_id]).groups
+    # XXX @groups not used in new ajax ui
+    @groups = @person.groups
     @groups.delete_if {|g| !g.adhoc_currency?}
 
     respond_to do |format|

app/controllers/memberships_controller.rb
@@ -9,6 +9,14 @@ class MembershipsController < ApplicationController
     end
   end
 
+  def show
+    @person = @membership.person
+    @account = @person.account(@membership.group)
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def edit
   end
   

app/controllers/people_controller.rb
@@ -36,8 +36,6 @@ class PeopleController < ApplicationController
       redirect_to home_url and return
     end
     if logged_in?
-      @some_contacts = @person.some_contacts
-      @common_contacts = current_person.common_contacts_with(@person)
       @groups = current_person == @person ? @person.groups : @person.groups_not_hidden
     end
     respond_to do |format|

app/helpers/application_helper.rb
@@ -185,14 +185,13 @@ module ApplicationHelper
     str << link_to_unless_current(action, path, opts)
   end
 
-  def exchange_link(person, options = {})
+  def exchange_link(person, group, options = {})
     img = image_tag("icons/switch.gif")
-    path = new_person_exchange_path(person)
-    opts = {}
+    path = new_person_exchange_path(person, :group => group.id)
     action = "Give credit"
-    str = link_to(img,path,opts)
+    str = link_to(img,path,options)
     str << " "
-    str << link_to_unless_current(action, path, opts)
+    str << link_to_unless_current(action, path, options)
   end
 
   def email_link(person, options = {})

app/helpers/memberships_helper.rb
@@ -1,2 +1,6 @@
 module MembershipsHelper
+  def membership_link(member, html_options = nil)
+    text = member.person.name
+    link_to(h(text), member, html_options)
+  end
 end

app/views/groups/_member.html.erb
@@ -1,10 +1,10 @@
 <% person = member.person %>
 <li class="vcard person">
   <div class="profile-image">
-    <%= image_link person, :class => "photo", :image => :thumbnail %>
+    <%= image_link person, :link_options => {:class => "show-follow"}, :image_options => {:class => "photo"}, :image => :thumbnail %>
   </div>
   <div class="col_content">
-    <p><%= person_link person, :class => "url fn" %></p>
+    <p><%= membership_link member, :class => "url fn show-follow" %></p>
     <% if defined? short %>
     <%= display person.listed_categories, :class => "meta" %>
     <% else %>

commit 738d819529fe22032bfe22250ace9841b4ea2128

groupy branch: clean up redundant code and fix regression in groups controller introduced in 0b0213464086bf5a005ed460e963f74ee71563f6

app/controllers/groups_controller.rb
@@ -80,6 +80,7 @@ class GroupsController < ApplicationController
     case params[:tab]
     when 'forum'
       @forum = @group.forum
+      @topics = Topic.find_recently_active(@forum, params[:page]) 
     when 'requests'
       @reqs = @group.reqs.paginate(:page => params[:page], :per_page => AJAX_POSTS_PER_PAGE)
     when 'offers'

app/views/groups/_forum_canvas.html.erb
@@ -1,4 +1,3 @@
-      <div id="forum_canvas">
         <h2><%= t('groups.show.discussion_topics') %><%= waiting_image %></h2>
         <ol class="list forum full">
           <%= render :partial => 'topics/topic', :collection => @topics %>
@@ -12,4 +11,3 @@
             </div>
           <% end %>
         <%- end -%>
-      </div>

app/views/groups/show.html.erb
@@ -27,7 +27,9 @@
       </div>
     <% end if @group.adhoc_currency? %>
     <% tab.create('forum', t('groups.show.forum')) do %>
-      <%= render :partial => 'forum_canvas' %>
+      <div id="forum_canvas">
+        <%= render :partial => 'forum_canvas' %>
+      </div>
       <%= render :partial => 'forum_form' %>
     <% end %>
     <% tab.create('requests', t('groups.show.requests')) do %>

public/javascripts/group.js
@@ -91,12 +91,6 @@ $(function() {
     return false;
     });
 
-  $('.topic a.show-follow').live('click',function() {
-    $('span.wait').show();
-    $.getScript(this.href);
-    return false;
-    });
-
   $('a[href=#forum]').bind('click',function() {
     $('span.wait').show();
     $.getScript(document.location.href+'?tab=forum');