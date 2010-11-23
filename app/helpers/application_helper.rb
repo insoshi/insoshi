d136440ec9a99eb7a249c524dfaf5174a6b137b7
Tom Brown <herestomwiththeweather@gmail.com>
2010-11-23 15:52:18 -0600

    groupy branch: improve partial to display a particular group member including showing credits for that currency. fix image links when listing group members.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -185,9 +185,9 @@ module ApplicationHelper
     str << link_to_unless_current(action, path, opts)
   end
 
-  def exchange_link(person, group, options = {})
+  def exchange_link(person, group = nil, options = {})
     img = image_tag("icons/switch.gif")
-    path = new_person_exchange_path(person, :group => group.id)
+    path = new_person_exchange_path(person, ({:group => group.id} unless group.nil?))
     action = "Give credit"
     str = link_to(img,path,options)
     str << " "
