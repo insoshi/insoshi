a294761244989db1b39f6bd6cc6061db85b2ea6e
Tom Brown <herestomwiththeweather@gmail.com>
2011-02-21 17:28:58 -0600

    groupy branch: fix admin navigation tabs and color. move jquery from application.js to site.js so it does not get included in admin mode

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,3 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(document).ready(function(){
-  $('#sfmenu li ul').css({
-    display: "none",
-    left: "auto"
-  });
-  $('#sfmenu li').hover(function() {
-    $(this)
-      .find('ul')
-      .stop(true, true)
-      .slideDown('fast');
-  }, function() {
-    $(this)
-      .find('ul')
-      .stop(true,true)
-      .fadeOut('fast');
-  });
-});
 
