9a32f7168ed13d169978a23fdae6e3ec4c090540
Tom Brown <herestomwiththeweather@gmail.com>
2011-03-08 18:32:46 -0600

    groupy branch: ...and set production caching to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ config.cache_store = :dalli_store
 
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = false
+config.action_controller.perform_caching             = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
