2a653db8f6d7eaaa1a96c45c9c7b70408f4c7c0b
Tom Brown <herestomwiththeweather@gmail.com>
2011-05-17 02:32:07 -0500

    groupy branch: demonstrate use of scopes that look like UMA action uris

--- app/models/client_application.rb
+++ app/models/client_application.rb
@@ -46,12 +46,21 @@ class ClientApplication < ActiveRecord::Base
   end
     
   def create_request_token(params={}) 
-    # valid scope (opentransact asset) is required!
-    group = Group.find_by_asset(params[:scope]) if params[:scope]
-    RequestToken.create(:client_application => self, 
-                        :scope => params[:scope], 
-                        :group_id => group.id, 
-                        :callback_url=>self.token_callback_url) unless group.nil?
+    # valid asset is required
+    if params[:scope]
+      scope_uri = URI.parse(params[:scope])
+      filepath = RAILS_ROOT + '/public' + scope_uri.path
+      if File.exist?(filepath)
+        scope_hash = CGI::parse(scope_uri.query)
+        group = Group.find_by_asset(scope_hash['asset'])
+        RequestToken.create(:client_application => self, 
+                            :scope => params[:scope], 
+                            :group_id => group.id, 
+                            :callback_url=>self.token_callback_url) unless group.nil?
+      else
+        logger.info "XXX create_request_token - file not found: #{scope_uri.path}"
+      end
+    end
   end
   
 protected
--- app/models/request_token.rb
+++ app/models/request_token.rb
@@ -9,7 +9,26 @@ class RequestToken < OauthToken
     self.verifier=OAuth::Helper.generate_key(20)[0,20] unless oauth10?
     self.save
   end
-  
+
+  def asset
+    scope_uri = URI.parse(self.scope)
+    scope_hash = CGI::parse(scope_uri.query)
+    scope_hash['asset']
+  end
+
+  # XXX assuming just one scope for now
+  def action_name
+    action['action']['name']
+  end
+
+  def action_icon_uri
+    action['action']['icon_uri']
+  end
+
+  def action
+    @action ||= JSON.parse(File.read(RAILS_ROOT + '/public' + URI.parse(self.scope).path))
+  end
+
   def exchange!
     return false unless authorized?
     return false unless oauth10? || verifier==provided_oauth_verifier
