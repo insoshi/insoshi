b4891dc603c69fdebf45bbcbc3d9c29d2d3820f1
Tom Brown <herestomwiththeweather@gmail.com>
2011-08-17 22:01:34 -0500

    rails3 branch: move populate categories to seeds.rb

--- db/migrate/20081028201811_populate_categories.rb
+++ db/migrate/20081028201811_populate_categories.rb
@@ -1,34 +1,7 @@
 class PopulateCategories < ActiveRecord::Migration
   def self.up
-    Category.reset_column_information
-    CATEGORIES.each do |value|
-      category = Category.new( :name => value, :description => "" )
-      category.save
-    end
   end
 
   def self.down
   end
-
-  CATEGORIES = [
-"Arts & Crafts",
-"Building Services",
-"Business & Administration",
-"Children & Childcare",
-"Computers",
-"Counseling & Therapy",
-"Food",
-"Gardening & Yard Work",
-"Goods",
-"Health & Personal",
-"Household",
-"Miscellaneous",
-"Music & Entertainment",
-"Pets",
-"Sports & Recreation",
-"Teaching",
-"Transportation",
-"Freebies",
-"Education"
-  ]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+  CATEGORIES = [
+"Arts & Crafts",
+"Building Services",
+"Business & Administration",
+"Children & Childcare",
+"Computers",
+"Counseling & Therapy",
+"Food",
+"Gardening & Yard Work",
+"Goods",
+"Health & Personal",
+"Household",
+"Miscellaneous",
+"Music & Entertainment",
+"Pets",
+"Sports & Recreation",
+"Teaching",
+"Transportation",
+"Freebies",
+"Education"
+  ]
+
+CATEGORIES.each do |value|
+  category = Category.find_or_create_by_name(value, :description => "")
+end
