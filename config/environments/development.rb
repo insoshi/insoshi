02116c5379ccd5e2daabcd90cf535794e7ca8d71
Tom Brown <herestomwiththeweather@gmail.com>
2011-11-20 03:09:50 -0600

    forgot to remove a line from dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 # Settings specified here will take precedence over those in config/environment.rb
 # XXX uncomment this for memcache
 #require 'active_support/cache/dalli_store23'
-require 'mail_view'
 
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
