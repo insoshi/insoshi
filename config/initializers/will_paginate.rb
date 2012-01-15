aee092dd63179b1677313e019b785e9b83fd192e
Tom Brown <herestomwiththeweather@gmail.com>
2012-01-14 23:57:26 -0600

    rails31 branch: fix will_paginate with rails_admin

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,12 @@
+# will_paginate issue #174
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        alias_method :per, :per_page
+        alias_method :num_pages, :total_pages
+        alias_method :total_count, :count
+      end
+    end
+  end
+end
