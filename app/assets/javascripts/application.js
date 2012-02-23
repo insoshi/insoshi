211079a931faa50aa4eb77b27707e6c888b01ba1
Tom Brown <herestomwiththeweather@gmail.com>
2012-02-22 21:34:25 -0600

    rails31 branch: add jquery_ujs. recompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 //= require jquery
+//= require jquery_ujs
 //= require jquery-ui-1.8.4.custom.min
 //= require jquery.timeago
 //= require highcharts
