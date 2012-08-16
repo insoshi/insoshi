b437b6664e4850e7fd757000263d6ed897f919f3
Jeff Aldrich <jgaldrich@gmail.com>
2012-08-16 10:59:16 -0500

    Added before_fork and after_fork to address Girl Friday/Unicorn exceptions.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,11 @@
 worker_processes 2 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true   # so things go good
+
+before_fork do |server, worker|
+	ActiveRecord::Base.connection_handler.clear_all_connections!
+end
+
+after_fork do |server, worker|
+	ActiveRecord::Base.connection_handler.verify_active_connections!
+end
