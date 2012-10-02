f09fb09f87f3b3d044c2441a072b3e6958766911
Jeff Aldrich <jgaldrich@gmail.com>
2012-10-02 12:23:49 -0500

    Issue #262 - show calculated balance (account.balance_with_initial_offset) in list and edit views.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -42,7 +42,11 @@ end
       field :offset do
         label "Starting Balance"
       end
-      field :balance 
+      field :balance do
+        formatted_value do
+          (bindings[:object].balance_with_initial_offset).to_s
+        end
+      end
       field :credit_limit
       field :updated_at do
         label "Last Transaction"
@@ -57,7 +61,11 @@ end
       field :offset do
         label "Starting Balance"
       end
-      field :balance
+      field :balance do
+        formatted_value do
+          (bindings[:object].balance_with_initial_offset).to_s
+        end
+      end
       field :credit_limit
     end
 
@@ -66,7 +74,11 @@ end
       field :offset do
         label "Starting Balance"
       end
-      field :balance
+      field :balance do
+        formatted_value do
+          (bindings[:object].balance_with_initial_offset).to_s
+        end
+      end
       field :credit_limit
       field :updated_at do
         label "Last Transaction"
