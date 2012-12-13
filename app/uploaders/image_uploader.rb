406c771782f6553e59c6ab87f5681664f5443359
Tom Brown <herestomwiththeweather@gmail.com>
2012-12-13 13:39:11 -0600

    rails329 branch: extension white list for carrierwave images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -51,9 +51,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
