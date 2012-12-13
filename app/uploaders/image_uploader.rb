6b789e95bcd2a2b2231daa3f64cbe7049ae61b59
Tom Brown <herestomwiththeweather@gmail.com>
2012-12-12 18:40:15 -0600

    rails329 branch: include Sprockets::Helpers

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
   storage :fog
