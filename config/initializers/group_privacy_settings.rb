715e05e4c13d48dc6a64417ef4e27545a11d7f6e
Tom Brown <herestomwiththeweather@gmail.com>
2013-11-13 17:25:34 -0600

    allow migration to work. related to gh-500

--- config/initializers/group_privacy_settings.rb
+++ config/initializers/group_privacy_settings.rb
@@ -1,7 +1,12 @@
+begin
 Group.all.each do |group|
   if group.privacy_setting.nil?
     p = PrivacySetting.new
     p.group = group
     p.save
   end
+end
+rescue
+  # Rescue from the error raised upon first migrating
+  nil
 end
