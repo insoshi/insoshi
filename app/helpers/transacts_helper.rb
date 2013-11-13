d6aaba7cd0571d2ed19766c0a3c78ee1a28e4310
Tom Brown <herestomwiththeweather@gmail.com>
2013-11-13 00:37:24 -0600

    display transaction values with two decimal places. closes gh-295

--- app/helpers/transacts_helper.rb
+++ app/helpers/transacts_helper.rb
@@ -6,9 +6,9 @@ module TransactsHelper
   def counterparty_action(t)
     units = t.group.nil? ? t('currency_unit_plural') : t.group.unit
     if current_person?(t.worker)
-      "+#{t.amount.to_s} #{units} (" + t('transacts.helper.received_from') + " "
+      "+#{number_with_precision(t.amount, precision: 2).to_s} #{units} (" + t('transacts.helper.received_from') + " "
     else
-      "-#{t.amount.to_s} #{units} (" + t('transacts.helper.paid_to') + " "
+      "-#{number_with_precision(t.amount, precision: 2).to_s} #{units} (" + t('transacts.helper.paid_to') + " "
     end
   end
 end
