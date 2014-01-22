119d89c160ff8c915906c8779f4d92545c3dfb3f
Ziyan Junaideen <jdeen-solutions@outlook.com>
2014-01-22 15:27:30 +0530

    Local Storage

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,7 +11,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
