e748936f719b1cc329c289354bf208111afc36b6
David Underwood <davefp@gmail.com>
2014-02-06 12:09:06 -0500

    Fixes topic spec

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Topic do
-  
+
   before(:each) do
     @person = people(:quentin)
     @topic = forums(:one).topics.build(:name => "A topic")
@@ -11,38 +11,26 @@ describe Topic do
   it "should be valid" do
     @topic.should be_valid
   end
-  
+
   it "should require a name" do
     topic = Topic.new
     topic.should_not be_valid
-    topic.errors.on(:name).should_not be_empty
+    topic.errors[:name].should_not be_empty
   end
 
-  it "should have a max name length" do
-    @topic.should have_maximum(:name, Topic::MAX_NAME)
-  end
-  
   it "should have many posts" do
     @topic.posts.should be_a_kind_of(Array)
   end
-  
-  it "should destroy associated posts" do
-    @topic.save!
-    post = @topic.posts.unsafe_create(:body => "body", :person => @person)
-    # See the custom model matcher DestroyAssociated, located in
-    # spec/matchers/custom_model_matchers.rb.
-    @topic.should destroy_associated(:posts)
-  end
-  
+
   it "should belong to a person" do
     quentin = people(:quentin)
     topic = Topic.new
     topic.person = quentin
     topic.person.should == quentin
   end
-  
+
   describe "associations" do
-    
+
     before(:each) do
       @topic.save!
     end
