7e60653f2efc8d47882137924c82a0cc468e2b06
dpolchlopek <dawid.polchlopek@polcode.com>
2014-03-31 16:20:00 +0200

    fix #543 Add validations to models, new look, order of fields

--- app/models/person_metadatum.rb
+++ app/models/person_metadatum.rb
@@ -3,4 +3,21 @@ class PersonMetadatum < ActiveRecord::Base
   attr_accessible *attribute_names, :as => :admin
 
   belongs_to :person
+  validate :allow_validation, :message => "This field is required"
+
+
+    def allow_validation
+      var = FormSignupField
+        .find(:first, :conditions => {:key => self.key})
+      if var.nil?
+        mandatory = false
+      else
+        mandatory = var.mandatory
+      end
+      if mandatory && self.value.empty?
+        # errors.add(self.key.to_sym, "#{self.key} field is required")
+        errors.add(key.to_sym, "hehehe some error")
+      end
+      binding.pry
+    end
 end
