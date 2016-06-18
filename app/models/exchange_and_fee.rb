37e4a56136f3c78a50ed46edd12e98cec2385e6d
Ziyan Junaideen <ziyan@jdeen.com>
2016-06-18 23:15:42 +0530

    Not charging if there marked true

--- app/models/exchange_and_fee.rb
+++ app/models/exchange_and_fee.rb
@@ -18,8 +18,8 @@
 
 class ExchangeAndFee < Exchange
   extend PreferencesHelper
-  after_create :withdraw_fee
-  after_destroy :destroy_relevant_fees
+  after_create :withdraw_fee, unless: :wave_all_fees
+  after_destroy :destroy_relevant_fees, unless: :wave_all_fees
 
   def withdraw_fee
     # group level transaction fees that apply to all group members
