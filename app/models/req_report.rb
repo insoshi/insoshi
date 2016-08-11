1da06d25b91c06682bfcde16d7f56f3cffd22e76
Ziyan Junaideen <ziyan@jdeen.com>
2016-08-11 22:00:21 +0530

    Annotate

--- app/models/req_report.rb
+++ app/models/req_report.rb
@@ -1,1 +1,14 @@
+# == Schema Information
+#
+# Table name: reports
+#
+#  id         :integer          not null, primary key
+#  type       :string(255)
+#  record     :string(255)
+#  person_id  :integer
+#  group_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ReqReport < Report; end
