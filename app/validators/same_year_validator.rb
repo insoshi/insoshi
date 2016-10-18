dc2bca26e8b5014fbe7edf7983d11aa0133fe19a
Ziyan Junaideen <ziyan@jdeen.com>
2016-10-18 23:45:36 +0530

    Validator fix + using validator in modal

--- app/validators/same_year_validator.rb
+++ app/validators/same_year_validator.rb
@@ -1,6 +1,6 @@
 class SameYearValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.year == Time.zone.now.year
+    unless value.try(:year) == Time.zone.now.year
       record.errors[attribute] << (options[:message] || 'is a different year')
     end
   end
