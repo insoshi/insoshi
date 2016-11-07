eec9b06d428ee2c8c6c8b353d1ad28b2c7a9d8fd
Ziyan Junaideen <ziyan@jdeen.com>
2016-11-07 20:27:10 +0530

    Validation failing transactions fixed

--- app/validators/same_year_validator.rb
+++ app/validators/same_year_validator.rb
@@ -1,5 +1,7 @@
 class SameYearValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    return unless value
+
     unless value.try(:year) == Time.zone.now.year
       record.errors[attribute] << (options[:message] || 'is a different year')
     end
