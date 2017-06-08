c4d0827bf9d34bbb52a0450d813f359d45f4f869
Ziyan Junaideen <ziyan@jdeen.com>
2017-06-08 17:02:50 +0530

    16 offers per page

--- app/controllers/public_offers_controller.rb
+++ app/controllers/public_offers_controller.rb
@@ -5,7 +5,9 @@ class PublicOffersController < ApplicationController
   skip_before_filter :require_activation
 
   def index
-    @offers = Offer.order('offers.id desc').paginate(page: params[:page])
+    @offers = Offer
+              .order('offers.id desc')
+              .paginate(page: params[:page], per_page: 16)
   end
 
   def show
