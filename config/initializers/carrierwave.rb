2f3d50cd7b31f9644768b4e0dc3c001f9936267a
Ziyan Junaideen <ziyan@jdeen.com>
2017-10-19 17:03:56 +0530

    Carrierwave cleanup

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,15 @@
 begin
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['AMAZON_ACCESS_KEY_ID'],       # required
-    :aws_secret_access_key  => ENV['AMAZON_SECRET_ACCESS_KEY'],       # required
-   }
-   config.fog_directory  = ENV['S3_BUCKET_NAME']                    # required
-   config.path_style = true
-   # config.fog_host       = "http://#{ENV['BUCKET']}.s3.amazonaws.com"            # optional, defaults to nil
-   # config.fog_host       = "http://"           # optional, defaults to nil
-   # config.fog_public     = false                                   # optional, defaults to true
-   config.fog_public = Preference.first.public_uploads unless Rails.env.test?
-   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-end
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AMAZON_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AMAZON_SECRET_ACCESS_KEY'],
+      endpoint: 'https://s3.amazonaws.com'
+    }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.path_style = true
+    config.fog_public = Preference.first.public_uploads unless Rails.env.test?
+  end
 rescue
-  # Rescue from the error raised upon first migrating
   nil
 end
